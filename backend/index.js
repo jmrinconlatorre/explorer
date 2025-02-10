3b15602658622193e137b49f457bc0a562b9dda1
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2025-02-10 16:37:06 +0100

    feat: added backend links

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,37 @@
+const { Web3 } = require('web3');
 const express = require('express');
-
 const app = express();
+const URL_INFURA =
+  'https://mainnet.infura.io/v3/df7ef1521c7043ea9381195e36e5c67f';
+
+const web3 = new Web3(URL_INFURA);
+
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 
 app.get('/', async (req, res) => {
-  await res.send('Hello world');
+  const bloque = (await web3.eth.getBlockNumber()).toString();
+  res.send({ bloque });
+});
+
+app.get('/bloque/:bloque', async (req, res) => {
+  const bloque = await web3.eth.getBlock(req.params.bloque);
+  res.send(bloque);
+});
+
+app.get('/tx/:tx', async (req, res) => {
+  const tx = await web3.eth.getTransaction(req.params.tx);
+  res.send(tx);
+});
+
+app.get('/balance/:address', async (req, res) => {
+  const balance = await web3.eth.getBalance(req.params.address);
+  res.send({
+    balance,
+    ethers: balance,
+    ethers2: web3.utils.fromWei(balance, 'ether'),
+  });
 });
 
 app.listen(3333);
